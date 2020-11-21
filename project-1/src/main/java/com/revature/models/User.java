c80266bbb2c42322c4dc41b5f0ef5c68a26fb084
Patrick Warring <pjwarring3@gmail.com>
2020-11-20 20:09:13 -0600

    User model

---
+++ project-1/src/main/java/com/revature/models/User.java
@@ -0,0 +1,91 @@
+package com.revature.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="users")
+public class User {
+
+	@Id @GeneratedValue
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="username", unique=true, nullable=false)
+	private String username;
+	
+	@Column(name="password", nullable=false)
+	private String password;
+	
+	@Column(name="firstname", nullable=false)
+	private String firstname;
+	
+	@Column(name="lastname", nullable=false)
+	private String lastname;
+	
+	@Column(name="role", nullable=false)
+	private String role;
+	
+	public User() {}
+	public User(int id, String username, String password, String firstname, String lastname, String role) {
+		this.id = id;
+		this.username = username;
+		this.password = password;
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.role = role;
+	}
+	public User(String username, String password, String firstname, String lastname, String role) {
+		this.username = username;
+		this.password = password;
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.role = role;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", username=" + username + ", password=" + password + ", firstname=" + firstname
+				+ ", lastname=" + lastname + ", role=" + role + "]";
+	}
+}
